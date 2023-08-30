3e648f14388e2aadddce57917db54719f148cc4b
yesu <119784057+yesubabu7@users.noreply.github.com>
2023-08-30 12:23:39 +0530

    Create ProductClass.java

---
+++ ProductClass.java
@@ -0,0 +1,65 @@
+public class Product {
+    private int prodId;
+    private String name;
+    private String description;
+    private String hsncode;
+    private String category;
+    private String imageUrl;
+
+    public Product(int prodId, String name, String description, String hsncode, String category, String imageUrl) {
+        this.prodId = prodId;
+        this.name = name;
+        this.description = description;
+        this.hsncode = hsncode;
+        this.category = category;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(int prodId) {
+        this.prodId = prodId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHsncode() {
+        return hsncode;
+    }
+
+    public void setHsncode(String hsncode) {
+        this.hsncode = hsncode;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
